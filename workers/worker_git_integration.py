318000f57fd5702d2ca821a4c53256865c54b20b
Andrew Brain <61482022+ABrain7710@users.noreply.github.com>
2021-07-27 19:18:34 -0500

    Add logging
    Signed-off-by: Andrew Brain <61482022+ABrain7710@users.noreply.github.com>

--- workers/worker_git_integration.py
+++ workers/worker_git_integration.py
@@ -267,9 +267,12 @@ class WorkerGitInterfaceable(Worker):
             }
         }
         self.logger.info("Made it 269")
-        table_values_cntrb = self.db.execute(
-            s.sql.select(self.get_relevant_columns(self.contributors_table,cntrb_action_map))
-        ).fetchall()
+        try:
+            table_values_cntrb = self.db.execute(
+                s.sql.select(self.get_relevant_columns(self.contributors_table,cntrb_action_map))
+            ).fetchall()
+        except:
+            self.logger.info("Error retrieving contributor data")
         # self.logger.info(f"table_values_cntrb: {table_values_cntrb}")
         self.logger.info("Made it 274")
 
